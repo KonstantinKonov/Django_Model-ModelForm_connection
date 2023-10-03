ff6cce052654f9977a7aa4c26093bde523d33201
Konstantin Konov <kostyakonov@gmail.com>
2023-10-03 22:36:43 +0300

    minor changes

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,7 @@ from app import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('create/', views.create, name='create'),
     path('update/<int:pk>', views.update, name='update'),
+    path('delete/<int:pk>', views.delete, name='delete'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,29 +1,50 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse, HttpResponseNotFound
 from app.forms import PersonForm
 from app.models import Person
 
 # Create your views here.
 
-# Create Read
+
+def create(request):
+    '''Create'''
+    if request.method == "GET":
+        return render(request, 'create.html', context={'title': 'Create page', 'form': PersonForm()})
+    else:
+        pf = PersonForm(request.POST)
+        if pf.is_valid():
+            Person.objects.create(**pf.cleaned_data)
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponse("bad data")
 
 
 def index(request):
-    if request.method == "GET":
-        return render(request, 'index.html', {
-            'title': 'Index page', 'form': PersonForm()})
-    else:
-        f = PersonForm(request.POST)
-        if f.is_valid():
-            p = Person.objects.create(**f.cleaned_data)
-            p.save()
-        return HttpResponseRedirect('/')
-
-# Update
+    '''Read'''
+    persons = Person.objects.all()
+    return render(request, 'index.html', context={
+        'title': 'Index page', 'persons': persons})
 
 
 def update(request, pk):
     if request.method == "GET":
-        pf = Person()
-        data = pf.objects.all.get(id=pk)
-        return render(request, 'update.html', {'title': 'Update page', 'form': PersonForm(data)})
+        try:
+            person = Person.objects.get(id=pk)
+        except Exception as e:
+            print(f"Exception {e} was risen while trying update a record")
+            return HttpResponseNotFound('Person not found')
+        return render(request, 'update.html', {'title': 'Update page', 'person': person, 'form': PersonForm()})
+    else:
+        pf = PersonForm(request.POST)
+        if pf.is_valid():
+            Person.objects.update(**pf.cleaned_data)
+            return HttpResponseRedirect('/')
+
+
+def delete(request, pk):
+    try:
+        Person.objects.get(id=pk).delete()
+        return HttpResponseRedirect('/')
+    except Exception as e:
+        print(f"Exception {e} was risen when trying delete record")
+        return HttpResponseNotFound('Person not found')
